f2a50f79d243dbb6fc3b72f62d6574cd1b240975
jacklmbrt07 <jacklmbrt07@gmail.com>
2021-03-04 18:22:27 -0800

    done

--- parseAndSortDates.js
+++ parseAndSortDates.js
@@ -1,33 +1,32 @@
 function parseAndSortDates(dataArray, delim = "/", isAsc = true) {
-  // data Array is an array of strings
-  // delim is a string, '/' if null. the character that seperates year, month, and day
-  // isAsc boolean. true for ascending order, false for descending, assume true.
-  // return an array of date objects parsed and sorted in ascending value
-  // if data array is undefined, return the empty array
-  // if a value in data array does not follow yyyymmdd format, delete it
-  // solution can be text or js file
-
-  // 1) first check if all the values are valid in dataArray, and remove it if it is not
-  //    1a) valid value would be in 'yyyy{delim}mm{delim}dd
-  // 2) if dataArray is empty, return []
-  // 3) add each value from dataArray into a newArray that is in datetime format
-  //    3a) if the datetime value doesnt exist, remove it also, for example 2020-12-32 should return an error
+  // 1) Declare variables, result will be returned, delim and isAsc are initialized as optional parameters.
   var result;
+
+  /* 2) We need to define a Regular Expression in order to filter  values with the format yyyy{delim}mm{delim}dd.
+    This regex will ONLY allow the years 1000 to 2999, the months 01 - 12 and days 01 - 31.
+    In case the delimeter is defined to other than '/', then we use interpolation to add ${delim} like a variable inside the RegExp. This is with ES6 only. 
+    To use variables in a regex, you must use the RegExp constructor, and replace /regex/ with `regex` and replace backslash '\' with a double backslash '\\'.
+
+    // source https://www.regextester.com/96683 but replaced '-' with a literal notation with the delim variable
+  */
   const dateRegex = new RegExp(
     `([12]\\d{3}${delim}(0[1-9]|1[0-2])${delim}(0[1-9]|[12]\\d|3[01]))`
   );
-  // source https://www.regextester.com/96683 but replaced '-' with a literal notation with the delim variable
 
+  // 3) DataArray needs to be filtered out, so if the string does not pass the regex test, it will be removed. stored in a new variable called filtered.
   let filtered = dataArray.filter((date) => dateRegex.test(date));
-  console.log("filtered: ", filtered);
 
+  // 4) if none of the values in dataArray pass, leaving filtered undefined or 0, we can immediately return an empty array.
   if (!filtered) return [];
 
+  // 5) "new Date()" constructor allows us to convert a string into a Date String.
+  // However, the string must have a delimeter of '/' or '-' in order to work, wo we use an if/else.
   if (delim == "/" || delim == "-") {
     result = filtered.map((date) => {
       return new Date(date);
     });
   } else {
+    // we use another regex in order to replace any improper delims with '/'. The "g" (global) will replace all instances.
     let delimRegex = new RegExp(delim, "g");
     result = filtered.map((date) => {
       let newDate = date.replace(delimRegex, "/");
@@ -35,10 +34,11 @@ function parseAndSortDates(dataArray, delim = "/", isAsc = true) {
     });
   }
 
+  // 6) Finally, we sort the result array into ascending or descending.
   if (isAsc) {
-    result = result.sort((a, b) => a - b);
+    result.sort((a, b) => a - b);
   } else {
-    result = result.sort((a, b) => b - a);
+    result.sort((a, b) => b - a);
   }
 
   return result;
@@ -51,4 +51,4 @@ var dataArray = [
   "1990~05~28",
   "junk",
 ];
-console.log(parseAndSortDates(dataArray, "-", false));
+console.log(parseAndSortDates(dataArray, "-", true));
